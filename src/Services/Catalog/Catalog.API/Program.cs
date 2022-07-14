11857740a02b8601995c3c92ff8909609bbccd77
minamorsali <mina_morsali@yahoo.com>
2022-07-14 11:11:35 +0430

    session 1 commit

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,25 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
+var configuration = GetConfiguration();
+var host = CreateHostBuilder(configuration, args);
+host.Run();
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+IConfiguration GetConfiguration()
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var path = Directory.GetCurrentDirectory();
+
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(path)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+    return builder.Build();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+IHost CreateHostBuilder(IConfiguration configuration, string[] args) =>
+    Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseStartup<Startup>()
+                  .UseContentRoot(Directory.GetCurrentDirectory())
+                  .UseWebRoot("Pics")
+                  .ConfigureAppConfiguration(x => x.AddConfiguration(configuration))
+                  .CaptureStartupErrors(false);
+    }).Build();
